5e7889f6a0483e2921dc2d7d489380688277ce59
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-03-02 05:21:10 +0000

    Task Date WIP

--- CH-3/topic-3/datetask.js
+++ CH-3/topic-3/datetask.js
@@ -1,12 +1,21 @@
-// Dapatkan tanggal hari ini. Kalau semisalnya ada yg berubah pakai let
-// const kalau tidak berubah
+// //Task 1
+// const today = new Date();
+
+// // Buat salinan tanggal hari ini, karena dayAgo akan berubah
+// let daysAgo = new Date(today);
+
+// // Kurangi 18 hari dari tanggal hari ini
+// daysAgo.setDate(today.getDate() - 18);
+
+// const dayString = daysAgo.toLocaleString('id-id', {  weekday: 'long' });
+// console.log(dayString);
+
+// Task 2
+// Tulislah kode untuk melihat hari dan tanggal lahir seseorang hanya dengan memasukkan umur
+
 const today = new Date();
+let age = 26;
+let birthDate = new Date(today.getFullYear() - age, today.getMonth(), today.getDate())
+let formattedBirthdate = birthdate.toLocaleDateString('en-US', options);
 
-// Buat salinan tanggal hari ini, karena dayAgo akan berubah
-let daysAgo = new Date(today);
-
-// Kurangi 18 hari dari tanggal hari ini
-daysAgo.setDate(today.getDate() - 18);
-
-const dayString = daysAgo.toLocaleString('id-id', {  weekday: 'long' });
-console.log(dayString);
+console.log(`Tanggal Lahirmu: ${birthDate}`)
