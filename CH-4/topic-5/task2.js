4bd4f7eb643c53b770319298fce2e425c5eb17c3
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-03-19 15:02:43 +0000

    Learning Object and Constructor

---
+++ CH-4/topic-5/task2.js
@@ -0,0 +1,37 @@
+let bandInfo;
+
+// Put your code here
+
+let job = "musician";
+
+bandInfo = {
+  name: "The Beatles",
+  nationality: "english",
+  genre: "rock",
+  members: 4,
+  formedYear: 1960,
+  split: true,
+  albums: [{
+      name: "Please Please Me",
+      released: 1960
+    },
+    {
+      name: "Let It Be",
+      released: 1970
+    }
+  ],
+  introduction: function() {
+    console.log(`${job} is a ${this.nationality} ${this.members} piece ${this.genre} band formed in ${this.formedYear}. 
+    Their first album is '${this.albums[0].name}' released in ${this.albums[0].released}`)
+  },
+  job: "guitarist"
+}
+
+bandInfo.introduction()
+
+
+// Don't edit the code below here
+
+let para1 = document.createElement('p');
+para1.textContent = bandInfo;
+section.appendChild(para1);
