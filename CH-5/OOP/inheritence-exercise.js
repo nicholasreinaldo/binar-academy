db699d1eea410dd0d40491a7b9bf90e31de6a385
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-04-05 04:07:43 +0000

    inheritence exercise

---
+++ CH-5/OOP/inheritence-exercise.js
@@ -0,0 +1,10 @@
+/*
+Create a base class called Shape which has a method calculateArea().
+Then, create two subclasses Circle and Rectangle that inherit from Shape. 
+Each subclass should implement its own version of calculateArea().
+
+Finally, create instances of Circle and Rectangle and demonstrate 
+the usage of inheritance by calling calculateArea() on each instance.
+
+*/
+
