b6b39dec000d70bfa945f09c0f7a3ff784a5ccf2
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-04-07 09:34:34 +0000

    inheritence exercise

--- CH-5/OOP/inheritence-exercise.js
+++ CH-5/OOP/inheritence-exercise.js
@@ -8,3 +8,39 @@ the usage of inheritance by calling calculateArea() on each instance.
 
 */
 
+class Shape {
+	// base implementation
+    calculateArea() {
+        throw new Error("Method 'calculateArea()' must be implemented.");
+    }
+
+}
+
+class Circle extends Shape {
+		constructor(radius) {
+        super();
+        this.radius = radius;
+    }
+
+   	 calculateArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(width, height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+    calculateArea() {
+        return this.width * this.height;
+    }
+
+}
+ 
+const circle = new Circle(7);
+console.log("Area of Circle:", circle.calculateArea()); 
+
+const rectangle = new Rectangle(4, 6);
+console.log("Area of Rectangle:", rectangle.calculateArea());
