a99d790cb1faa29a5402ca8e869a8705b13a74af
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-04-28 07:37:12 +0000

    Apr 28, 2024, 7:37 AM

--- CH-6/express-exercise/index.js
+++ CH-6/express-exercise/index.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 // Buat sebuah http method get
-app.get('/', (req,res) => res.send("Test"));
+app.get('/', (req, res) => res.send('Test'))
 
-app.get('/binar', (req,res) => res.send("Hello world"));
+app.get('/binar', (req, res) => res.send('Hello world'))
 
-app.listen(port,() => console.log(`Server is running at http://localhost:${port}`));
+app.get('/products', (req, res) =>
+  res.json([
+    {
+      name: 'Iphone',
+      qty: 10,
+    },
+    {
+      name: 'samsung',
+      qty: 3,
+    },
+  ]),
+)
+
+app.get('/services', (req, res) => [
+  { name: 'Tukang komputer' },
+  { name: 'Ojol' },
+])
+
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`),
+)
