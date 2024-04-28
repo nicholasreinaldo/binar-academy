a8497816699e5f8500c34540d80da3e430d1804c
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-04-28 09:43:01 +0000

    Apr 28, 2024, 9:43 AM

--- CH-6/zoo-management/index.js
+++ CH-6/zoo-management/index.js
@@ -6,7 +6,10 @@ const port = 3000;
 let animals = generateAnimals();
 
 app.get("/animals", (req,res) => res.json(animals));
-app.get("/animals/:id")
+app.get("/animals/:id", (req,res) => {
+    const id = req.params.id;
+    console.log(id);
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
