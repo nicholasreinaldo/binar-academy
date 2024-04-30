c94addf207f78fcd6d8129383b4a2cb5d8f8825a
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-04-30 12:28:48 +0000

    Apr 30, 2024, 12:28 PM

--- CH-6/bingle-shop/index.js
+++ CH-6/bingle-shop/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const product = require('./db/products.json')
+
+let balance = 100000000
+let cart = {}
+
+app.get('/products', (req, res) => res.json(products))
 
 app.listen(port, () =>
   console.log(`Server is running at http://localhost:${port}`),
