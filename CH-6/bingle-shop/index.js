6a1e327f727489b0c079b23fbccfb478ee1cf612
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-04-30 14:58:54 +0000

    Practicing ejs

--- CH-6/bingle-shop/index.js
+++ CH-6/bingle-shop/index.js
@@ -6,8 +6,27 @@ const products = require('./db/products.json')
 let balance = 100000000
 let cart = {}
 
-app.get('/products', (req, res) => res.json(products))
+router.post('/api/cart', (req, res) => {
+  const { productId, quantity } = req.body
+  const product = products.find((p) => p.id === productId)
+  if (!product) {
+    return res.status(404).json({ message: 'Product tidak terdaftar' })
+  }
 
-app.listen(port, () =>
-  console.log(`Server is running at http://localhost:${port}`),
-)
+  if (quantity > product.stock) {
+    return res.status(400).json({ message: 'Stock tidak mencukupi' })
+  }
+})
+
+router.get
+
+const productsString = JSON.stringify(products)
+
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
+app.get('/products', (req, res) => res.json(products))
+app.get('/', (req, res) => res.render('index', { products }))
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`)
+})
