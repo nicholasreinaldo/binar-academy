e6a324159819774cf5e91918382ab5fb9166706d
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-05-05 07:23:58 +0000

    May 5, 2024, 7:23 AM

--- CH-6/bingle-shop/index.js
+++ CH-6/bingle-shop/index.js
@@ -4,9 +4,9 @@ const port = 8080
 const products = require('./db/products.json')
 const cartRoutes = require('./routes/cart.route')
 
+app.use(express.json())
 app.use(cartRoutes)
 app.use(express.static(__dirname + '/public'))
-app.use(express.json())
 app.set('view engine', 'ejs')
 app.get('/api/products', (req, res) => res.json(products))
 
--- CH-6/bingle-shop/routes/cart.route.js
+++ CH-6/bingle-shop/routes/cart.route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const products = require('../db/products.json')
 let balance = 100000000
 let cart = {}
 
