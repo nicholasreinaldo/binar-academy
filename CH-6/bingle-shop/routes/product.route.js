826daa35c5cc545830fd71a1352aefb3313a72fb
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-05-05 07:12:02 +0000

    May 5, 2024, 7:12 AM

--- CH-6/bingle-shop/routes/product.route.js
+++ CH-6/bingle-shop/routes/product.route.js
@@ -1,4 +1,32 @@
 const express = require('express')
 const router = express.Route()
 
+app.get('/api/cart', (req, res) => {
+  console.log(req.body)
+  const { productId, quantity } = req.body
+  const product = products.find((p) => p.id === productId)
+  if (!product) {
+    return res.status(404).json({ message: 'Product tidak terdaftar' })
+  }
+  if (quantity > product.stock) {
+    return res.status(400).json({ message: 'Stock tidak mencukupi' })
+  }
+  if (cart[productId]) {
+    if (cart[productId].quantity + quantity > product.stock) {
+      return res.status(400).json({ message: 'Stock tidak mencukupi' })
+    }
+    cart[productId].quantity += quantity
+    cart[productId].total += quantity * product.price
+  } else {
+    cart[productId] = {
+      name: product.name,
+      description: product.description,
+      brand: product.brand,
+      price: product.price,
+      quantity: quantity,
+      total: (product.price = quantity),
+    }
+  }
+})
+
 module.export = router
