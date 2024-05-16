2f9d38614ac3528d13fa330bc44d17257fe25ded
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-05-16 12:57:14 +0000

    May 16, 2024, 12:57 PM

--- CH-7/library-api/config/database.js
+++ CH-7/library-api/config/database.js
@@ -2,7 +2,7 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    port: 3306,
+    port: 5432,
     user: 'postgres',
     password: '1223',
     database: 'library_db',
