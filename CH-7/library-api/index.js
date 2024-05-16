15a4aa4ee5030e0223a61b14e99c779bc1a924fa
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-05-16 12:56:11 +0000

    May 16, 2024, 12:56 PM

--- CH-7/library-api/index.js
+++ CH-7/library-api/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const knex = require('./config/database')
+
+app.get('/books', async (req, res) => {
+  const books = await knex('books').select('*')
+  return res.json(books)
+})
 
 app.listen(port, () =>
   console.log(`Server is running at http://localhost:${port}`),
