d37edadcecaeda8b6f8619fb1e5673a511519cd5
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-06-11 14:14:20 +0000

    Jun 11, 2024, 2:14 PM

--- CH-9/topic-1/react-app/src/App.jsx
+++ CH-9/topic-1/react-app/src/App.jsx
@@ -2,7 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return <></>
+  const [foods, setFoods] = useState(['Apple', 'Orange', 'Banana'])
+
+  function handleAddFood() {
+    const newFood = document.getElementById('foodInput').value
+    document.getElementById('foodInput').value = ''
+    setFoods((f) => [...f, newFood])
+  }
+
+  return (
+    <div>
+      <input type="text" id="foodInput" placeholder="Enter first name" />
+      <button onClick={handleAddFood}>Add Food</button>
+
+      <h2>List of Food</h2>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index}>{food}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
