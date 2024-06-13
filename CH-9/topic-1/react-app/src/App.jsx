b23ed29e2c74f5e299f6d77df22e8629617f4860
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-06-13 14:31:41 +0000

    Jun 13, 2024, 2:31 PM

--- CH-9/topic-1/react-app/src/App.jsx
+++ CH-9/topic-1/react-app/src/App.jsx
@@ -1,8 +1,56 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const fetchProfiles = async () => {
+  try {
+    const response = await fetch('https://randomuser.me/api/?results=10')
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.error('Error fetching the profiles:', error)
+    return []
+  }
+}
+
 function App() {
-  return <div></div>
+  const [profiles, setProfiles] = useState([])
+
+  useEffect(() => {
+    const getProfiles = async () => {
+      const fetchedProfiles = await fetchProfiles()
+      setProfiles(fetchedProfiles)
+    }
+
+    getProfiles()
+  }, [])
+
+  return (
+    <div className="container">
+      <h1 className="text-center my-4">User Profiles</h1>
+      <div className="row">
+        {profiles.map((profile, index) => (
+          <div key={index} className="col-md-4 mb-4">
+            <div className="card">
+              <img
+                src={profile.picture.medium}
+                className="card-img-top"
+                alt={`${profile.name.first} ${profile.name.last}`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{`${profile.name.title} ${profile.name.first} ${profile.name.last}`}</h5>
+                <p className="card-text">Gender: {profile.gender}</p>
+                <p className="card-text">Email: {profile.email}</p>
+                <p className="card-text">
+                  Location:{' '}
+                  {`${profile.location.city}, ${profile.location.state}, ${profile.location.country}`}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
