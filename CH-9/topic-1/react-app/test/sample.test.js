fb2a60eab2bfd39509b58e97716b8aaf34f014d4
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-06-25 13:10:03 +0000

    Jun 25, 2024, 1:10 PM

--- CH-9/topic-1/react-app/test/sample.test.js
+++ CH-9/topic-1/react-app/test/sample.test.js
@@ -1,35 +1,38 @@
 const sum = require('../src/utils/sum')
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
+describe('sum function tests', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
 
-test('adds 5 + 6 to equal 11', () => {
-  expect(sum(5, 6)).toBe(11)
-})
+  it('adds 5 + 6 to equal 11', () => {
+    expect(sum(5, 6)).toBe(11)
+  })
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).not.toBe(13)
-})
+  it('adds 1 + 2 to not equal 13', () => {
+    expect(sum(1, 2)).not.toBe(13)
+  })
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+  it('adds 1 + 2 to equal 3 (duplicate test 1)', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
+
+  it('adds 1 + 2 to equal 3 (duplicate test 2)', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
 })
 
 describe('checking string', () => {
   const name = 'Samuel Anugerah Zega'
 
-  test('first test - checking name parts', () => {
+  it('should check name parts', () => {
     const arrName = name.split(' ')
     expect(arrName[0]).toBe('Samuel')
     expect(arrName[1]).toBe('Anugerah')
     expect(arrName[2]).toBe('Zega')
   })
 
-  test('second test - checking name length', () => {
+  it('should check name length', () => {
     expect(name.length).toBe(20)
   })
 })
