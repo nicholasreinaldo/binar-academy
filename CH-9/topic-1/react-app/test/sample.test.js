547d4a9721fed73a0b2cde2d70c88f88ca851fbf
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-06-25 13:01:25 +0000

    Jun 25, 2024, 1:01 PM

--- CH-9/topic-1/react-app/test/sample.test.js
+++ CH-9/topic-1/react-app/test/sample.test.js
@@ -18,3 +18,13 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
+test('checking string', () => {
+  const name = 'Samuel Anugerah Zega'
+  test('first test', () => {
+    const arrName = name.split()
+    expect(arrName[1]).toBe('Samuel')
+    expect(arrName[1]).toBe('Anugerah')
+    expect(arrName[1]).toBe('Zega')
+  })
+})
