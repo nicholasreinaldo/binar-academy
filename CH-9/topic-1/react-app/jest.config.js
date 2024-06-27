81cb67f1906ab6d3c9ee7dda0d0a4b728f082e8e
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-06-27 13:46:14 +0000

    Jun 27, 2024, 1:46 PM

--- CH-9/topic-1/react-app/jest.config.js
+++ CH-9/topic-1/react-app/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    preset: 'vite-js',
-    testEnvironment: 'jsdom'
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg|css)$': 'jest-transform-stub',
+  },
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
 }
