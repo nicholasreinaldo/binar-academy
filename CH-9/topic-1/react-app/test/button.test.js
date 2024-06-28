3a8a5b7cb62402c38b67a48021bbaabe3609c040
Nicholas Reinaldo <122370292+nicholasreinaldo@users.noreply.github.com>
2024-06-28 12:57:06 +0000

    Jun 28, 2024, 12:57 PM

--- CH-9/topic-1/react-app/test/button.test.js
+++ CH-9/topic-1/react-app/test/button.test.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { render } from '@vitejs/plugin-react'
+import { render, fireEvent } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
+import '@testing-library/jest-dom'
+
 import Button from '../src/components/button'
 
 test('Button rendered correctlu and responds to click event', () => {
   const label = 'Button to Click'
   const handleClick = jest.fn()
-  render(<Button label={label} handleClick={handleClick} />)
+  render(<Button label={label} onClick={handleClick} />)
 
-  const button = screen.getByTest(label)
+  const button = screen.getByText(label)
   expect(button).toBeInTheDocument()
+
+  fireEvent.click(button)
+  expect(handleClick).toHaveBeenCalledTimes(1)
 })
